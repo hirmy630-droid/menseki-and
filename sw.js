3e4cf9ddf7e4963e408c4d6cd61b0edafbc88089
hirmy630-droid <hirmy630@gmail.com>
2026-03-30 08:51:48 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'area-calc-pwa-v4';
-const CORE_ASSETS = [
+const CACHE_NAME = 'area-calc-pwa-v5';
+const APP_SHELL = [
   './',
   './index.html',
   './manifest.webmanifest',
@@ -10,7 +10,7 @@ const CORE_ASSETS = [
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(CORE_ASSETS))
+      .then((cache) => cache.addAll(APP_SHELL))
       .then(() => self.skipWaiting())
   );
 });
@@ -31,17 +31,20 @@ self.addEventListener('fetch', (event) => {
   if (event.request.method !== 'GET') return;
 
   const url = new URL(event.request.url);
-  const isSameOrigin = url.origin === self.location.origin;
-  const isNavigation = event.request.mode === 'navigate';
+  if (url.origin !== self.location.origin) return;
 
-  if (!isSameOrigin) return;
+  const isNavigation = event.request.mode === 'navigate';
 
   if (isNavigation) {
     event.respondWith(
-      fetch(event.request)
+      fetch(event.request, { cache: 'no-store' })
         .then((response) => {
-          const copy = response.clone();
-          caches.open(CACHE_NAME).then((cache) => cache.put('./index.html', copy)).catch(() => {});
+          if (response && response.ok) {
+            const copy = response.clone();
+            caches.open(CACHE_NAME).then((cache) => {
+              cache.put('./index.html', copy).catch(() => {});
+            });
+          }
           return response;
         })
         .catch(() => caches.match('./index.html'))
@@ -55,8 +58,11 @@ self.addEventListener('fetch', (event) => {
 
       return fetch(event.request)
         .then((response) => {
+          if (!response || !response.ok) return response;
           const copy = response.clone();
-          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy)).catch(() => {});
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, copy).catch(() => {});
+          });
           return response;
         });
     })
