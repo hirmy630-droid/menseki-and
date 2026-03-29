d0c41d914b70fa49a7888c10e4759cbf4c26c769
hirmy630-droid <hirmy630@gmail.com>
2026-03-30 08:54:05 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'area-calc-pwa-v5';
-const APP_SHELL = [
+const CACHE_NAME = 'area-calc-pwa-v4';
+const CORE_ASSETS = [
   './',
   './index.html',
   './manifest.webmanifest',
@@ -10,7 +10,7 @@ const APP_SHELL = [
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(APP_SHELL))
+      .then((cache) => cache.addAll(CORE_ASSETS))
       .then(() => self.skipWaiting())
   );
 });
@@ -31,20 +31,17 @@ self.addEventListener('fetch', (event) => {
   if (event.request.method !== 'GET') return;
 
   const url = new URL(event.request.url);
-  if (url.origin !== self.location.origin) return;
-
+  const isSameOrigin = url.origin === self.location.origin;
   const isNavigation = event.request.mode === 'navigate';
+
+  if (!isSameOrigin) return;
 
   if (isNavigation) {
     event.respondWith(
-      fetch(event.request, { cache: 'no-store' })
+      fetch(event.request)
         .then((response) => {
-          if (response && response.ok) {
-            const copy = response.clone();
-            caches.open(CACHE_NAME).then((cache) => {
-              cache.put('./index.html', copy).catch(() => {});
-            });
-          }
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put('./index.html', copy)).catch(() => {});
           return response;
         })
         .catch(() => caches.match('./index.html'))
@@ -58,11 +55,8 @@ self.addEventListener('fetch', (event) => {
 
       return fetch(event.request)
         .then((response) => {
-          if (!response || !response.ok) return response;
           const copy = response.clone();
-          caches.open(CACHE_NAME).then((cache) => {
-            cache.put(event.request, copy).catch(() => {});
-          });
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy)).catch(() => {});
           return response;
         });
     })
