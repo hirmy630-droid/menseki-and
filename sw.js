ef2dcd6278ce8deae3daf0d7064853cd2a3d9403
hirmy630-droid <hirmy630@gmail.com>
2026-03-30 19:44:35 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-const CACHE_NAME = 'area-calc-pwa-v7';
+const CACHE_NAME = 'area-calc-pwa-v8';
 const APP_SHELL = [
   './',
   './index.html',
-  './manifest.webmanifest?v=20260330-scrollfix2',
-  './icon-192.png?v=20260330-scrollfix2',
-  './icon-512.png?v=20260330-scrollfix2',
-  './icon-maskable-192.png?v=20260330-scrollfix2',
-  './icon-maskable-512.png?v=20260330-scrollfix2'
+  './manifest.webmanifest?v=20260330-final',
+  './icon-192.png?v=20260330-final',
+  './icon-512.png?v=20260330-final',
+  './icon-maskable-192.png?v=20260330-final',
+  './icon-maskable-512.png?v=20260330-final'
 ];
 
 self.addEventListener('install', (event) => {
